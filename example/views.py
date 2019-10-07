4315af724602db68844966c7cd6e3239a6a00852
Ihitoman <163211@ids.upchiapas.edu.mx>
2019-10-07 09:36:40 -0500

    deploy

--- example/views.py
+++ example/views.py
@@ -122,7 +122,9 @@ class UsersList(APIView):
     def post(self, request, format=None):
         serializer = UserSerializer(data = request.data)
         if serializer.is_valid():
-            serializer.save()
+            user = User.objects.create_user(request.data)
+            user.save()
+            #serializer.save()
             datas = serializer.data
             return Response(datas)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
